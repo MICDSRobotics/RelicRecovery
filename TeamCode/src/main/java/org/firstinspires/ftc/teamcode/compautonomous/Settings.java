138f3985bfc84e58424a8885d4ca57eabe86caf3
Blake Abel <blakecabel@gmail.com>
2018-01-18 19:20:45 -0600

    Updated Settings ith full battery values, and made it wiggle wiggle wiggle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compautonomous/Settings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/compautonomous/Settings.java
@@ -6,10 +6,22 @@ package org.firstinspires.ftc.teamcode.compautonomous;
  * @since 12/30/17
  */
 public interface Settings {
+
+    //Full battery tests
+    int firstStretch = 1300;
+    int forwardShort = 950;
+    int sideShort = 750;
+    int rotate90 = 475;
+    //int lineUp = ;
+    double slamIntoWallSpeed = 0.4;
+
+
+    /* FIRST TEST VALUES (FULL BATTERY?)
     int firstStretch = 1400;
     int forwardShort = 1050;
     int sideShort = 900;
     int secondStretch = 1000;
     int rotate90 = 549; //2196 for a full rotation
     double slamIntoWallSpeed = 0.4;
+    */
 }
