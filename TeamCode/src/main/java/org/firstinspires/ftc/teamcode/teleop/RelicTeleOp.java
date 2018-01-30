b2eeb3ca2e79ad2af12775d0be6487a575f511db
Blake Abel <blakecabel@gmail.com>
2018-01-29 18:37:34 -0600

    More documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RelicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/RelicTeleOp.java
@@ -47,17 +47,9 @@ import static org.firstinspires.ftc.teamcode.robotplus.gamepadwrapper.Controller
 
 
 /**
- * This file contains an example of an iterative (Non-Linear) "OpMode".
- * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
- * The names of OpModes appear on the menu of the FTC Driver Station.
- * When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all iterative OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Main Teleop Program for Competition (with Relic Grabber
+ * @author Blake A, Alex M
+ * @since 1/4/2018
  */
 
 @TeleOp(name="Relic Grabbing", group="YUH YUH")
