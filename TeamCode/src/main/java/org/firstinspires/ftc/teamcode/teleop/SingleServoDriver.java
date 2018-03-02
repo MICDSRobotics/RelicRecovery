b075e869354d2fed138b2f091f42cf948105f452
stlgolfer <amigala@micds.org>
2018-03-02 10:23:02 -0600

    Single Servo Mods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SingleServoDriver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SingleServoDriver.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
