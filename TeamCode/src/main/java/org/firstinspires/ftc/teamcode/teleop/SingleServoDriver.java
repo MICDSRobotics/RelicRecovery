bdb3e64d3f9647f6e53494cfa701faeeb1af9e27
stlgolfer <amigala@micds.org>
2018-03-01 22:35:33 -0600

    SingleServo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SingleServoDriver.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.robotplus.gamepadwrapper.Controller;
+
+/**
+ * Created by amigala on 3/1/2018.
+ */
+@TeleOp(name = "SingleServo", group = "Test")
+public class SingleServoDriver extends OpMode {
+    private Servo singleServo;
+    private Controller p1;
+
+    @Override
+    public void init() {
+        this.singleServo = hardwareMap.get(Servo.class, "servo");
+        this.p1 = new Controller(gamepad1);
+    }
+
+    @Override
+    public void loop() {
+        if (p1.dpadUp.isDown()) {
+            this.singleServo.setPosition(this.singleServo.getPosition() + 0.01);
+        }
+        else if (p1.dpadDown.isDown()) {
+            this.singleServo.setPosition(this.singleServo.getPosition() - 0.01);
+        }
+
+        telemetry.addData("Position", singleServo.getPosition());
+
+        p1.update();
+    }
+}
